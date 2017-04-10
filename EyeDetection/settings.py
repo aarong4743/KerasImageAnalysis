a3f86ee4c4808e559b261b5a77dcaf4124f12818
Patrick Hayes <patrickghhayes@gmail.com>
2017-04-10 11:22:21 -0700

    Created App

--- EyeDetection/settings.py
+++ EyeDetection/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'EyeDetection.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
